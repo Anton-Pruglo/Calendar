9c5e338983f97d561b9ca1bc62600cf12004c43a
Anton-Pruglo <xatastudio@gmail.com>
2021-06-17 11:25:03 +0300

    finish redact

--- src/components/Calendar/components/DayMonth/DayMonth.jsx
+++ src/components/Calendar/components/DayMonth/DayMonth.jsx
@@ -1,34 +1,54 @@
 import React from 'react';
+import getDay from "date-fns/getDay";
+import startOfWeek from 'date-fns/startOfWeek'
+import getDaysInMonth from "date-fns/getDaysInMonth";
 import classnames from 'classnames';
-import { getData } from './utils'
 
-import './DayMonth.css'
-
+import './DayMonth.css';
 
 export class DayMonth extends React.Component {
-  static defaultProps = {
-    date: new Date(),
-    weekDayNames: ['Mo', 'Tu', 'We', 'Th', 'Fri', 'Sa', 'Su'],
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date(),
+      currentDate: new Date(),
+    }
   }
-  state = {
-    date: this.props.date,
-    currentDate: new Date(),
-  };
-
   get year() {
     return this.state.date.getFullYear();
   }
   get month() {
     return this.state.date.getMonth();
   }
-
   get day() {
     return this.state.date.getDate();
   }
 
+  getDays = (year, month) => {
+    const result = [];
+    const date = new Date(year, month);
+    const daysInMonth = getDaysInMonth(date);
+    const monthStartsOn = getDay(date);
+    let day = 1;
+
+    for (let i= 0; i < (daysInMonth + monthStartsOn) / 7; i++) {
+      result[i] = [];
+      for(let j = 0; j < 7; j++) {
+        if ((i === 0 && j < monthStartsOn) || day > daysInMonth) {
+          result[i][j] = undefined;
+        } else {
+          result[i][j] = new Date(year, month, day++);
+        }
+      }
+    }
+    return result;
+  }
+
+
   render() {
     const { currentDate } = this.state;
-    const monthData = getData.Day(this.year, this.month);
+    const monthData = this.getDays(this.year, this.month);
+
     return (
 
         <table className='dayMonth'>
@@ -42,7 +62,8 @@ export class DayMonth extends React.Component {
                     })}
                   >{date.getDate()}</td>
                     :
-                  <td key={index}/>
+                  <td key={index}
+                    className='notCurrentMonth'/>
                 )}
               </tr>
             )}
--- src/components/Calendar/components/DayMonth/utils/index.js
+++
@@ -1,1 +0,0 @@
-export  * as getData  from './getDate';
